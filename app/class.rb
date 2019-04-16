15aa401bb45f17ac621c73a10ff424a424a13a9b
rafamorais <rafaelsilvademorais@gmail.com>
2019-04-15 22:46:57 -0300

    finalizando atributos e metodos

--- app/class.rb
+++ app/class.rb
@@ -1,14 +1,37 @@
 class Person
     
-    attr_reader :age
-    attr_writer :age
+    def initialize(n)
+        @name = n
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
     
+    # attr_reader :age
+    # attr_writer :age
+
+    attr_accessor :age
+    
+    def say(name)
+        puts 'I am talking with ' + name
+    end
+
 end
 
 # puts Person.instance_variables
 # puts Person.class_variables
 
-rafa = Person.new
+rafa = Person.new('João')
+
+rafa.name = 'Rafa Morais'
+puts rafa.name
 
 rafa.age = 32
 puts rafa.age
+
+# rafa.say('Alca')
