fd95377aeaa2817c3ae30c2d7bd3631c4765e427
rafamorais <rafaelsilvademorais@gmail>
2019-04-17 10:30:03 -0300

    finalizando overide

--- app/animal.rb
+++ app/animal.rb
@@ -27,10 +27,16 @@ class Dog < Animal
     def latir
         puts 'Eu sei Latir'
     end
+
+    def walk
+        puts 'I am walking with ' + @paws.to_s + ' paws and i am Dog'
+    end
+
+
 end
 
 
 dog = Dog.new('Bob', 2.0, 4)
 
 dog.walk
-dog.latir
+# dog.latir
